c52b98c74f523b1fc1b0d8dbcd3d1598bb4267cf
sanjib007 <loton1984@gmail.com>
2018-08-04 01:15:03 +0600

    Oauth2 Setup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('client')->except('index1');
+        $this->middleware('auth')->except('index');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'hello world';
+    }
+
+    public function index1()
+    {
+        return 'uguygu hello world';
+    }
+}
